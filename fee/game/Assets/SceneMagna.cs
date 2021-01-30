ff496e3260b9e27ce7ba261b29efed4edce32fb2
Jesaxe <78266125+Jesaxe@users.noreply.github.com>
2021-01-30 18:44:06 -0500

    menu

---
+++ fee/game/Assets/SceneMagna.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMagna : MonoBehaviour
+{
+    public GameObject creditos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditos.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Inicio()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Creditos()
+    {
+        creditos.gameObject.SetActive(true);
+    }
+    public void Atras()
+    {
+        creditos.gameObject.SetActive(false);
+    }
+}
