da8953adc92c74863c8b08b0a7e4fae096f2b656
Jesaxe <78266125+Jesaxe@users.noreply.github.com>
2021-01-31 13:07:37 -0500

    ea

--- Assets/Scripts/Rodar.cs
+++ Assets/Scripts/Rodar.cs
@@ -7,11 +7,14 @@ public class Rodar : MonoBehaviour
 {
     public float velocidad;
     public Rigidbody2D rb;
+
     public GameObject pies1,pies2,mano1,mano2,ojos,piso;
     public bool pies,manos,ojitos;
     public float velocidad2;
-   
-
+    public float salto;
+    public Vector3 offset;
+    bool agarrado;
+    bool puedeag;
     void Start()
     {
         
@@ -26,47 +29,78 @@ public class Rodar : MonoBehaviour
         mano2.SetActive(false);
         ojos.SetActive(false);
         piso.SetActive(false);
+        agarrado = false;
+        puedeag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movY = Input.GetAxis("Vertical");
         Vector3 movimeinto = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
-        
-        
-        if(pies==false)
+
+        if (!agarrado)
         {
-            transform.Rotate (new Vector3 (0, 0, Input.GetAxis("Horizontal")*-80) * Time.deltaTime);
-            transform.position += movimeinto *velocidad*Time.deltaTime;
-        }
-        if(pies==true)
-        {
-            piso.SetActive(true);
-            transform.rotation = Quaternion.identity;
-            
-            pies1.SetActive(true);
-            pies2.SetActive(true);
-            transform.position += movimeinto *velocidad*Time.deltaTime*velocidad2;
-            if(Input.GetAxis("Horizontal")!=0)
+            if (pies == false)
             {
-                if(Input.GetAxis("Horizontal")>=0)
+                transform.Rotate(new Vector3(0, 0, Input.GetAxis("Horizontal") * -80) * Time.deltaTime);
+                transform.position += movimeinto * velocidad * Time.deltaTime;
+            }
+            if (pies == true)
+            {
+                piso.SetActive(true);
+                transform.rotation = Quaternion.identity;
+
+                pies1.SetActive(true);
+                pies2.SetActive(true);
+                transform.position += movimeinto * velocidad * Time.deltaTime * velocidad2;
+                if (Input.GetAxis("Horizontal") != 0)
                 {
-                    transform.localScale=new Vector3(0.35f,0.35f,1);
+                    if (Input.GetAxis("Horizontal") >= 0)
+                    {
+                        transform.localScale = new Vector3(0.35f, 0.35f, 1);
+                    }
+                    else
+                    {
+                        transform.localScale = new Vector3(-0.35f, 0.35f, 1);
+                    }
                 }
+                if (Input.GetButtonDown("Jump"))
+                {
+                    saltar();
+                }
+
+            }
+        }
+        if (agarrado)
+        {
+            rb.velocity = new Vector2(0, Input.GetAxis("Vertical")) * velocidad;
+
+            offset.y += movY / 10;
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (movY < 0)
+                {
+                    seSuelta();
+                }
+
                 else
                 {
-                    transform.localScale=new Vector3(-0.35f,0.35f,1);
+                    seSuelta();
+                    saltar();
                 }
             }
-                
-
         }
-        if(manos)
+        if (manos)
         {
             mano1.SetActive(true);
             mano2.SetActive(true);
+            puedeag = true;
             //codigo de escalar
+        }
+        else if (!manos)
+        {
+            puedeag = false;
         }
         if(ojitos)
         {
@@ -89,11 +123,46 @@ public class Rodar : MonoBehaviour
         }
         if (collision.gameObject.tag == "Pies")
         {
+            print(".");
             pies=true;
             collision.gameObject.SetActive(false);
         }
     }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "ropes")
+        {
+            if (puedeag)
+            {
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    agarrado = true;
 
-    
+                    rb.isKinematic = true;
+                }
+            }
+            
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "ropes")
+        {
+            agarrado = false;
+            rb.isKinematic = false;
+        }
+    }
+    void seSuelta()
+    {
+        agarrado = false;
+        offset = Vector2.zero;
+        rb.isKinematic = false;
+    }
+    void saltar()
+    {
+        rb.AddForce(new Vector2(0, salto), ForceMode2D.Impulse);
+    }
+
+
 
 }
